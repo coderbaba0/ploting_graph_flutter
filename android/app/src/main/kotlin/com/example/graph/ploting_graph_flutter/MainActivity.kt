8728db3d80de0907a2e7360b0ca275b2bdaa8001
coderbaba <sattababakingup@gmail.com>
2023-07-26 11:50:01 +0530

    Initial commit by coderbaba

---
+++ android/app/src/main/kotlin/com/example/graph/ploting_graph_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.graph.ploting_graph_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'graphplot.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Graph play demo flutterfever.com',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: GraphPlay(),
+    );
+  }
+}
+
